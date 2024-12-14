509f2611dacc64e180e594044080937efb157109
Yash Agrawal <yashagrawal81930@gmail.com>
2024-12-15 01:07:03 +0530

    modal finished

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+Array.from(buttons).map(button => {
+  button.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+closeModalBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape') closeModal();
+});
